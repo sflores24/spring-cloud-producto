3823af1f06ee77b7d2663a93cc4691e94555c13e
FLORES SANCHEZ <salvador.flores-sanchez@capgemini.com>
2020-03-03 10:09:06 -0600

    add errors for try hystrix

--- src/main/java/mx/com/springboot/app/productos/controllers/ProductoController.java
+++ src/main/java/mx/com/springboot/app/productos/controllers/ProductoController.java
@@ -15,28 +15,38 @@ import mx.com.springboot.app.productos.models.services.IProductoService;
 
 @RestController
 public class ProductoController {
-	
+
 	@Autowired
 	private Environment env;
-	
+
 	@Value("{server.port}")
 	private String port;
 
 	@Autowired
 	private IProductoService productoService;
-	
+
 	@GetMapping("/listar")
 	public List<Producto> listar() {
 		return productoService.findAll().stream().map(prod -> {
-			prod.setPort(Integer.parseInt(env.getProperty("local.server.port"))); //Con environment
+			prod.setPort(Integer.parseInt(env.getProperty("local.server.port"))); // Con environment
 			return prod;
 		}).collect(Collectors.toList());
 	}
-	
+
 	@GetMapping("/ver/{id}")
-	public Producto detalle(@PathVariable Long id) {
+	public Producto detalle(@PathVariable Long id) throws Exception {
 		Producto producto = productoService.findById(id);
 		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+
+//Ej 1 Hystrix
+//		boolean ok = false;
+//		if(ok == false) {
+//			throw new Exception("Exception of test");
+//		}
+
+//Ej 2 hystrix para configuración de timeout
+		// Thread.sleep(2000);
+
 		return producto;
 	}
 }
